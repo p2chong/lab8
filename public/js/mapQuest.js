abef4acdd66340876e5efc914aa799a01b9e3ca9
Paul Chong <p2chong@ucsd.edu>
2019-02-28 10:24:56 -0800

    lab8

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
+	L.mapquest.key = 'uvbIkzpfVMz7m19eNYxCA1dl6ooIaK1M';
+
+	var map = L.mapquest.map('map',{
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 12,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 	// add your code here
 }
